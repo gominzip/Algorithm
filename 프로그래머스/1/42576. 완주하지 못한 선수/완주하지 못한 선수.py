d3f5bdc1f1c2688ccd6f960d2f198e75fce9380e
MinJi <101329724+gominzip@users.noreply.github.com>
2024-03-13 03:48:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.88 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -6,12 +6,11 @@ def solution(participant, completion):
         if result.get(p) is None :
             result[p] = 1
         else :
-            result[p] += 1  # 동명이인 고려
+            result[p] += 1
     
     for c in completion :
-        result[c] -= 1  # 완주한 사람은 value가 0
+        result[c] -= 1
     
-    # 딕셔너리 리버스, value로 key 찾기
     reverse = {v:k for k,v in result.items()}
     answer = reverse.get(1)
         
