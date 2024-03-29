9b397d1038d39dff9740d68d1b81eabeac52927a
MinJi <101329724+gominzip@users.noreply.github.com>
2024-03-29 13:19:35 +0900

    [level 2] Title: 오픈채팅방, Time: 68.87 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,19 @@
+def solution(record):
+    answer = []
+    user_dic = {}
+    for data in record :
+        line = data.split()
+        if(line[1] in user_dic) :
+            if(line[0]=='Change' or line[0]=='Enter') :
+                user_dic[line[1]] = line[2]
+        else :
+            user_dic[line[1]] = line[2]
+            
+    for data in record :
+        line = data.split()
+        if(line[0]=='Enter'):
+            answer.append(user_dic[line[1]] + '님이 들어왔습니다.')
+        if(line[0]=='Leave'):
+            answer.append(user_dic[line[1]] + '님이 나갔습니다.')
+            
+    return answer
