15dcd05dbf5c0d8bc8af1ce64a34e0b7fdf0ebca
MinJi <101329724+gominzip@users.noreply.github.com>
2024-04-16 20:11:20 +0900

    [level 3] Title: 야근 지수, Time: 217.61 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,38 @@
+from queue import PriorityQueue
+
+def solution(n, works):
+    answer = 0
+    dic = {}
+    for work in works:
+        if work not in dic:
+            dic[work] =1
+        else : dic[work] +=1
+        
+    while n>0:
+        max_key = max(dic.keys())
+        
+        if max_key == 0:
+            return 0
+        
+        if dic[max_key] < n:
+            n -= dic[max_key]
+
+            if max_key-1 not in dic:
+                dic[max_key-1] =dic[max_key]
+            else : dic[max_key-1] += dic[max_key]
+            
+            del dic[max_key]
+            
+        else:
+            dic[max_key] -=n
+            
+            if max_key-1 not in dic:
+                dic[max_key-1] = n
+            else : dic[max_key-1] +=n
+            
+            n = 0
+        
+    print(dic)
+    for key in dic.keys():
+        answer += (key**2)*dic[key]
+    return answer
