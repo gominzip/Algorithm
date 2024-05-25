2148a8ef648e85b566f213505cf8ea7fea88da4e
MinJi <101329724+gominzip@users.noreply.github.com>
2024-05-25 17:49:31 +0900

    [level 3] Title: 단속카메라, Time: 2.46 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -2,7 +2,6 @@ function solution(routes) {
     var answer = 0;
     routes.sort((a,b)=>a[1]-b[1])
     current = -30001
-    console.log(routes)
     for (let route of routes){
         if(route[0]>current){
             current = route[1];
