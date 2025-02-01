b8f524146eaa159942abca047eca22e025aaa936
MinJi Ko <101329724+gominzip@users.noreply.github.com>
2025-02-01 19:19:08 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 29.61 ms, Memory: 49.5 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,26 +1,22 @@
 function solution(enroll, referral, seller, amount) {
     var answer = [];
-    const tree = {'center': { profit: 0, referral: null }};
-    // 수익과 추천인 초기화
-    enroll.map((v,i)=>{
-        tree[v] = {profit: 0, referral: referral[i] === '-'? 'center' : referral[i]}
-    })
-    // 판매수익 수수료 계산 및 수익 반영
-    seller.map((v,i)=>{
-        profit = amount[i]*100
-        while(tree[v].referral){
-            if(!profit) break;  // 수익 없을 시 종료
-            charge = parseInt(profit * 0.1);
-            tree[v].profit += profit - charge;
+    const tree = new Map([["-", { ref: "", profit: 0 }]]);
+    
+    enroll.forEach((name, i) => {
+        tree.set(name, { ref: referral[i], profit: 0 });
+    });
+
+    seller.forEach((name, i) => {
+        let curr = name;
+        let profit = amount[i] * 100;
+
+        while (curr && profit > 0) {
+            let charge = Math.floor(profit * 0.1);
+            tree.get(curr).profit += (profit - charge);
+            curr = tree.get(curr).ref;
             profit = charge;
-            v = tree[v].referral;
         }
-        tree[v].profit += profit;
-    })
-    
-    enroll.map((v)=>{
-        answer.push(tree[v].profit);
-    })
-    
-    return answer;
+    });
+
+    return enroll.map(name => tree.get(name).profit);;
 }
