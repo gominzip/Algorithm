9b929adc17eb8e0d0db08fc7dc0845a2b9e78153
MinJi Ko <101329724+gominzip@users.noreply.github.com>
2025-02-13 00:33:38 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 41.71 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,19 +1,22 @@
 function solution(gems) {
     var answer = [];
-    const gem_map = new Map();
-    const gem_size = new Set(gems).size;
+    let distance = Infinity
+    const n = new Set(gems).size;
+    const window = new Map()
     
-    gems.forEach((gem,i)=>{
-        gem_map.delete(gem);
-        gem_map.set(gem,i); // key:보석 이름, value:현재 인덱스
-        
-        if(gem_map.size === gem_size){
-            // map의 첫번째 요소와 마지막요소(현재 위치)를 push
-            answer.push([gem_map.values().next().value+1,i+1]);
+    let start = 0;
+    for (let end = 0; end < gems.length; end++) {
+        window.set(gems[end], (window.get(gems[end]) || 0) + 1);
+        while(window.size === n){
+            if(end - start < distance){
+                distance = end - start;
+                answer = [start + 1, end + 1];
+            }
+            
+            window.set(gems[start], window.get(gems[start]) - 1);
+            if (window.get(gems[start]) === 0) window.delete(gems[start]);
+            start++;
         }
-    })
-    
-    answer.sort((a,b)=> (a[1]-a[0]) - (b[1]-b[0]))
-
-    return answer[0];
+    }
+    return answer;
 }
