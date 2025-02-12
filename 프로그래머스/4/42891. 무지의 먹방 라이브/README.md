42c8498b6c053416a32efa0294d03a052ea143d5
MinJi Ko <101329724+gominzip@users.noreply.github.com>
2025-02-13 01:41:35 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 192.82 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/README.md
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/README.md
@@ -1,10 +1,10 @@
 # [level 4] 무지의 먹방 라이브 - 42891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42891) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42891#) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 270.37 ms
+메모리: 62.2 MB, 시간: 192.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 13일 01:39:21
+2025년 02월 13일 01:41:33
 
 ### 문제 설명
 
--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
@@ -1,92 +1,26 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  push(value) {
-    this.heap.push(value);
-    this._heapifyUp();
-  }
-
-  pop() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-    
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this._heapifyDown();
-    return min;
-  }
-
-  peek() {
-    return this.heap[0] || null;
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  _heapifyUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[parentIndex].time <= this.heap[index].time) break;
-      
-      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
-      index = parentIndex;
-    }
-  }
-
-  _heapifyDown() {
-    let index = 0;
-    const length = this.heap.length;
-
-    while (true) {
-      let leftChild = index * 2 + 1;
-      let rightChild = index * 2 + 2;
-      let smallest = index;
-
-      if (leftChild < length && this.heap[leftChild].time < this.heap[smallest].time) {
-        smallest = leftChild;
-      }
-      if (rightChild < length && this.heap[rightChild].time < this.heap[smallest].time) {
-        smallest = rightChild;
-      }
-      if (smallest === index) break;
-
-      [this.heap[smallest], this.heap[index]] = [this.heap[index], this.heap[smallest]];
-      index = smallest;
-    }
-  }
-}
-
 function solution(food_times, k) {
-  let totalFoodTime = 0;
-  const heap = new MinHeap();
-  
-  food_times.forEach((time, idx) => {
-    totalFoodTime += time;
-    heap.push({ time, idx: idx + 1 });
-  });
+    var answer = 0;    
+    let max_time = 0;
+    const queue = food_times.map((time, idx)=> {
+        max_time += time;
+        return {time, idx: idx+1}
+    });
+    
+    if (max_time <= k) return -1;
+    
+    queue.sort((a,b) => b.time - a.time);
+    
+    let total = 0;
+    let prev = 0;
+    let size = queue.length;
+    
+    while(total + (queue[size-1].time - prev) * size <= k){
+        const pick = queue.pop().time;
+        total += (pick - prev) * size;
+        size --;
+        prev = pick;
+    }
 
-  if (totalFoodTime <= k) return -1;
-
-  let acc = 0;
-  let prev = 0;
-  let remainSize = heap.size();
-
-  while (heap.size() > 0 && acc + (heap.peek().time - prev) * remainSize <= k) {
-    const pick = heap.pop();
-    acc += (pick.time - prev) * remainSize;
-    remainSize--;
-    prev = pick.time;
-  }
-
-  const restFoods = [];
-  while (heap.size() > 0) {
-    restFoods.push(heap.pop());
-  }
-  restFoods.sort((a, b) => a.idx - b.idx);
-
-  return restFoods[(k - acc) % remainSize].idx;
+    queue.sort((a, b) => a.idx - b.idx);
+    return queue[(k - total) % size].idx;
 }
