b824108b0fc0c9934994a87d30c537f63d3f590d
MinJi Ko <101329724+gominzip@users.noreply.github.com>
2025-08-03 19:03:13 +0900

    [level 1] Title: 체육복, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,40 +1,26 @@
 function solution(n, lost, reserve) {
-    var answer = n - lost.length;
-    const infoMap = new Map();
+    const clothes = Array(n + 1).fill(1);
+
+    const realLost = lost.filter(l => !reserve.includes(l));
+    const realReserve = reserve.filter(r => !lost.includes(r));
     
-    for(let i = 1; i <= n; i++){
-        infoMap.set(i, 1);
-    }
-    
-    for(let l of lost){
-        infoMap.set(l, 0);
-    }
-    
-    for(let r of reserve){
-        let clothes = infoMap.get(r) + 1
-        infoMap.set(r, clothes);
-        // 분실했지만 여분이 있는 학생
-        if(clothes === 1) answer++;
-    }
+    realLost.forEach(l => clothes[l]--);
+    realReserve.forEach(r => clothes[r]++);
     
     // 그리디를 위해서는 번호순 정렬을 해야함
-    lost.sort((a,b)=>a-b);
+    realLost.sort((a,b) => a - b);
     
-    for(let l of lost){
-        if(infoMap.get(l)) continue
+    for(let l of realLost){
         let [front, back] = [l-1, l+1]
-        if(infoMap.get(front) > 1){
-            infoMap.set(front, 1);
-            infoMap.set(l, 1);
-            answer++;
-            continue;
-        }
-        if(infoMap.get(back) > 1){
-            infoMap.set(back, 1);
-            infoMap.set(l, 1);
-            answer++;
-            continue;
+        
+        if(front > 0 && clothes[front] === 2){
+            clothes[front]--;
+            clothes[l]++;
+        } else if(back <= n && clothes[back] === 2){
+            clothes[back]--;
+            clothes[l]++;
         }
     }
-    return answer;
+    
+    return clothes.slice(1).filter(c => c > 0).length;
 }
